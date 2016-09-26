3b50f74a1c7c44f8c82418af9740cf13db76e312
Gabriel <gdominguez@devspark.com>
2016-09-26 12:53:59 -0300

    details

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,23 @@ router.get('/', function(req, res, next) {
   	res.render('index', { title: 'Express', info: data });
 });
 
-/* GET Userlist page. */
 router.get('/data', function(req, res) {
     var db = req.db;
     var collection = db.get('schema_info');
     collection.find({},{},function(e,data){
         res.render('data', {
+            "data" : data
+        });
+    });
+});
+
+router.get('/details/:id', function(req, res) {
+    var db = req.db;
+    var val = req.params.id;
+    var collection = db.get('schema_info');
+    collection.findOne({name: val},function(e,data){
+    	console.log(data);
+        res.render('details', {
             "data" : data
         });
     });
