da0106cc989755de60b1c4517183c808c210447d
Gabriel <gdominguez@devspark.com>
2016-09-27 20:42:39 -0300

    add save

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ router.get('/data', function(req, res) {
     var db = req.db;
     var collection = db.get('schema_info');
     collection.find({},{},function(e,data){
-    	for (var i = data.length - 1; i >= 0; i--) {
+    	for (var i =  0; i < data.length; i++) {
     		var suma1 = 0;
     		var suma2 = 0;
     		var suma3 = 0;
-    		for (var k = data[i].data.length - 1; k >= 0; k--) {
+    		for (var k = 0; k < data[i].data.length; k++) {
     			suma1 = suma1 + data[i].data[k].values[0];
     			suma2 = suma2 + data[i].data[k].values[1];
     			suma3 = suma3 + data[i].data[k].values[2];
@@ -23,7 +23,6 @@ router.get('/data', function(req, res) {
     		var totales = {first : suma1, second: suma2, third: suma3};
     		data[i].totales = totales;
     	}
-    	console.log(data);
         res.render('data', {"data" : data});
     });
 });
@@ -38,5 +37,34 @@ router.get('/details/:id', function(req, res) {
         });
     });
 });
+
+router.post('/details/:id', function(req, res) {
+    var db = req.db;
+    var val = req.params.id;
+    var collection = db.get('schema_info');
+    collection.findOne({name: val},function(e,data){
+        var arr_res = [];
+        var arr_data = data.data;
+        for (var i = 0; i < arr_data.length; i++) {
+            var result = {};
+            result.id = arr_data[i].id;
+            result.name = arr_data[i].name; 
+            result.values = []; 
+            var v1 = 'ponderacion_'+arr_data[i].id;
+            var v2 = 'grado_'+arr_data[i].id;
+            var v3 = 'result_'+arr_data[i].id;
+            result.values.push(parseInt(eval('req.body.'+v1)));
+            result.values.push(parseInt(eval('req.body.'+v2)));
+            result.values.push(parseInt(eval('req.body.'+v3)));
+            arr_res.push(result);
+        }
+        collection.update({_id:data._id}, {$set : {data: arr_res}}, function(err, result) {
+            if (err)
+                console.log(result);
+        });
+        res.redirect('/data');
+    });
+});
+
 
 module.exports = router;
